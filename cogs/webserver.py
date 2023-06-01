6d7ebbbf7ae768a9a3d40f8ad63ce1b96e8c92d0
logix <74055355+anyastrophic@users.noreply.github.com>
2023-06-01 09:05:20 +0300

    Update webserver.py

--- cogs/webserver.py
+++ cogs/webserver.py
@@ -169,7 +169,9 @@ async def verify_endpoint(request: Request, verification_request: VerificationRe
     await Registration(discord_id, roblox_id).links()
 
     member: discord.Member = BOT.ub_guild.get_member(discord_id)
-    print(BOT, member)
+    for member in BOT.ub_guild.members:
+        if member.id == 405821998221230080:
+            print(member)
     role: discord.Role = BOT.ub_guild.get_role(406997457709432862)
 
     if role not in member.roles:
