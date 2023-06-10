3c44cf2344856385dc1be14b014e7606372c4a52
Anya <74055355+anyastrophic@users.noreply.github.com>
2023-06-10 11:25:56 +0300

    Update roblox_utils.py

--- modules/roblox_utils.py
+++ modules/roblox_utils.py
@@ -1,18 +1,20 @@
 import os
 
+from aiohttp import ClientResponseError
+
 COOKIE = os.environ.get("ROBLOX_COOKIE")
 
 
 async def get_csrf(client):
-    result = await client.post(
-        "https://auth.roblox.com",
-        headers={"Cookie": f".ROBLOSECURITY={COOKIE}"},
-    )
+    try:
+        await client.post(
+            "https://auth.roblox.com",
+            headers={"Cookie": f".ROBLOSECURITY={COOKIE}"},
+        )
+    except ClientResponseError as exc:
+        headers = exc.headers
+        csrf_token = headers.get("x-csrf-token")
 
-    json = result.json.as_dict()
+        assert csrf_token is not None, "csrf token is None"
 
-    csrf_token = json.get("x-csrf-token")
-
-    assert csrf_token is not None, "csrf token is None"
-
-    return csrf_token
+        return csrf_token
